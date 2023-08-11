3310771b4c9c5221df4929e4fe27343961273ed0
Arlen D'Arcy <arlenjdarcy@gmail.com>
2023-08-10 20:16:36 -0500

    fix fonts

--- installs/install-powerline-fonts.sh
+++ installs/install-powerline-fonts.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-git clone https://github.com/powerline/fonts.git --depth=1
+# git clone https://github.com/powerline/fonts.git --depth=1
+git clone https://github.com/ryanoasis/nerd-fonts.git
 cd fonts
 ./install.sh
 cd ..
-rm -rf fonts
-
-
-# also download this https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/RobotoMono.zip
+rm -rf nerd-fonts
